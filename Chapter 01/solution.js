7e708b4b37689ccda6ad3a2ba8f947218b819772
Sandip Sharma <sandipsharma38536@gmail.com>
2023-07-07 19:46:23 +0530

    Create solution.js
    solution of practice set 1

---
+++ Chapter 01/solution.js
@@ -0,0 +1,36 @@
+// Q 1
+let variable = "String";
+let variable2 = 45;
+let result = variable + variable2;
+console.log(result);
+
+// Q 2
+console.log(typeof variable)
+
+// Q 3
+const obj = {
+  name: "Sandip",
+  subject: "JavaScript",
+  level: "bigginer"
+}
+
+// obj = 45;
+// console.log(obj)
+
+// Q 4
+obj['subject'] = "Python"
+console.log(obj)
+obj['subject'] = "JavaScript"
+obj['level'] = "Pro"
+console.log(obj)
+
+// Q 5
+const dic = {
+  apple: "a round fruit with firm, white flesh and a green, red, or yellow skin",
+  appreciate: "recognize the full worth of.",
+  ataraxia: "a state of freedom from emotional disturbance and anxiety",
+  yakka: "work, especially hard work.",
+  ball: "a round object that you hit, kick, throw, etc. in games and sports"
+}
+
+console.log(dic["ball"])
