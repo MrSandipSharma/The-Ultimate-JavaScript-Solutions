ae62e23549f71d15b54ed6d14eeb8177f8143248
Sandip Sharma <sandipsharma38536@gmail.com>
2023-07-07 21:04:10 +0530

    Create solution.js
    solution..js

---
+++ Chapter 02/solution.js
@@ -0,0 +1,59 @@
+// Q 1
+// let age = prompt("Enter your age : ");
+// age = Number.parseInt(age);
+// if(age > 10 && age < 20){
+//   console.log("Your age lies between 10 and 20");
+// }
+// else{
+//   console.log("Your age don't lie between 10 and 20");
+// }
+
+// Q 2
+// switch(new Date().getDay()){
+//   case 0:
+//     console.log("Sunday")
+//     break;
+//   case 1:
+//     console.log("Monday")
+//     break;
+//   case 2:
+//     console.log("Tuesday")
+//     break;
+//   case 3:
+//     console.log("Wednesday")
+//     break;
+//   case 4:
+//     console.log("Thursday")
+//     break;
+//   case 5:
+//     console.log("Friday")
+//     break;
+//   case 0:
+//     console.log("Saturday")
+//     break;
+// }
+
+// Q 3
+// let num = prompt("Enter a number :")
+// num = Number.parseInt(num)
+// if (num % 2 == 0 && num % 3 == 0){
+//   console.log("The Number is divisible by 2 and 3")
+// }
+// else{
+//   console.log("The Number is not divisible by 2 and 3")
+// }
+
+// Q 4
+// let num = prompt("Enter a number :")
+// num = Number.parseInt(num)
+// if (num % 2 == 0 || num % 3 == 0){
+//   console.log("The Number is divisible by either 2 and 3")
+// }
+// else{
+//   console.log("The Number is neither divisible by 2 and 3")
+// }
+
+// Q 5
+let a = prompt("Enter your age :");
+let result = a > 18 ? "You can Drive" : "You can not Drive";
+console.log(result);
