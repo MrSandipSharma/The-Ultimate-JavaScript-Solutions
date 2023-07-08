713ec176d5bfb601dee0d8b5d1f8e4d72a39ab26
Sandip Sharma <sandipsharma38536@gmail.com>
2023-07-08 12:34:49 +0530

    Create solution.js

---
+++ Chapter 06/solution.js
@@ -0,0 +1,37 @@
+// Q 01
+// let age = prompt("Enter your age :")
+// age = Number.parseInt(age);
+// let alrt = age > 18 ? alert("You can drive"): alert("You can not drive")
+
+// Q 02
+// let runagain = true;
+// while(runagain == true){
+//   let age = prompt("Enter your age :")
+//   age = Number.parseInt(age);
+//   let alrt = age > 18 ? alert("You can drive"): alert("You can not drive")
+//   runagain = confirm("would you wanna change your age ?")
+// };
+
+// Q 03
+// let runagain = true;
+// while(runagain == true){
+//   let age = prompt("Enter your age :")
+//   age = Number.parseInt(age);
+//   if (age < 0){
+//     console.error("Please type your age again");
+//     break;
+//   }
+//   let alrt = age > 18 ? alert("You can drive"): alert("You can not drive")
+//   runagain = confirm("would you wanna change your age ?")
+// };
+
+// Q 04
+// let age = prompt("Enter your age :");
+// age = Number.parseInt(age);
+// if (age > 4){
+//   location.href = "https://google.com/"
+// }
+
+// Q 5
+let clr = prompt("Enter a color name :")
+document.body.style.background = clr
